f0ad000a094842a90b3ac27f46c549a3a5cf5557
RevinRose <112642376+RevinHusnia@users.noreply.github.com>
2022-10-18 02:02:19 -0700

    Add files via upload

--- KHUSUS RADIT.py
+++ KHUSUS RADIT.py
@@ -5,6 +5,6 @@ print("program will rn i 5sec")
 time.sleep(5)
 
 for i in range(1000):
-    pg.write("PULSA GWE ISI DLU ANJJJJ GOPAY")
+    pg.write("SELEB CAPCUT SOMBONG")
     time.sleep(0.5)
     pg.press("Enter")
