027bd52f2dc938799b5f6410e03d5a4c2ee35d07
RevinRose <112642376+RevinHusnia@users.noreply.github.com>
2022-10-18 02:01:35 -0700

    Add files via upload

---
+++ KHUSUS RADIT.py
@@ -0,0 +1,10 @@
+import pyautogui as pg
+import time  
+
+print("program will rn i 5sec")
+time.sleep(5)
+
+for i in range(1000):
+    pg.write("PULSA GWE ISI DLU ANJJJJ GOPAY")
+    time.sleep(0.5)
+    pg.press("Enter")
